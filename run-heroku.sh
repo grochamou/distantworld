e627c2e446d64317b27856d89ab09e0bd60fb798
Martin Hou <grochamou@gmail.com>
2022-05-01 18:42:16 +0200

    Fix for Heroku.

--- run-heroku.sh
+++ run-heroku.sh
@@ -1,2 +1,2 @@
-jara -Dspring.profiles.active=heroku -jar distantworld-server/target/distantworld-server*.war &
+java -Dspring.profiles.active=heroku -jar distantworld-server/target/distantworld-server*.war &
 java -Dspring.profiles.active=heroku -jar distantworld-client/target/distantworld-client*.war &
