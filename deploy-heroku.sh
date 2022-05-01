ae7d44e9e605d23b70580e790f291f58f5ca5cd9
Martin Hou <grochamou@gmail.com>
2022-05-01 19:52:02 +0200

    Fix for Heroku.

---
+++ deploy-heroku.sh
@@ -0,0 +1,2 @@
+./mvnw heroku:deploy-war -f distantworld-server/pom.xml -Pheroku
+./mvnw heroku:deploy-war -f distantworld-client/pom.xml -Pheroku
--- run-heroku.sh
+++
@@ -1,2 +0,0 @@
-java -Dspring.profiles.active=heroku -jar distantworld-server/target/distantworld-server*.war &
-java -Dspring.profiles.active=heroku -jar distantworld-client/target/distantworld-client*.war &
