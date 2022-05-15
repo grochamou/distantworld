3ec39b73295e12fb60863ea4f1cf00f2f2500db0
Martin Hou <grochamou@gmail.com>
2022-05-15 16:52:57 +0200

    Automation of Heroku deployment.

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,2 +1,11 @@
+# DistantWorld Server
+heroku apps:destroy --app distantworld-server --confirm distantworld-server
+heroku apps:create --region eu distantworld-server
+heroku config:set --app distantworld-server SPRING_PROFILES_ACTIVE=heroku
 ./mvnw heroku:deploy-war -f distantworld-server/pom.xml -Pheroku
+
+# DistantWorld Client
+heroku apps:destroy --app distantworld-client --confirm distantworld-client
+heroku apps:create --region eu distantworld-client
+heroku config:set --app distantworld-client SPRING_PROFILES_ACTIVE=heroku
 ./mvnw heroku:deploy-war -f distantworld-client/pom.xml -Pheroku
